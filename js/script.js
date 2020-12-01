228d57e9dd1b7e8ce09096f85d0160b5389ea4f1
Estefania <elopez17@hawk.iit.edu>
2020-12-01 11:32:43 -0600

    added nav to map

--- js/script.js
+++ js/script.js
@@ -13,14 +13,67 @@ function initMap(){
     });
 }
 */
+
+const favemap = {
+  loganSquare: {
+    center: { lat: 41.92771741920748, lng: -87.70252134482129 },
+    population: 2714856,
+  },
+  Bridgeport: {
+    center: { lat: 40.714, lng: -74.005 },
+    population: 8405837,
+  },
+  RoscoeVil: {
+    center: { lat: 34.052, lng: -118.243 },
+    population: 3857799,
+  },
+  park: {
+    center: { lat: 49.25, lng: -123.1 },
+    population: 603502,
+  },
+};
+
 var map;
 function initMap() {
+  alert('it works initMap');
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 41.895033518464885, lng: -87.63399272412327},
+    zoom: 11,
+  });
+  // Construct the circle for each value in citymap.
+  // Note: We scale the area of the circle based on the population.
+  for (const spot in favemap) {
+    alert('it enters for');
+    // Add the circle for this city to the map.
+    var cityCircle = new google.maps.Circle({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map: map,
+      center: favemap[spot].center,
+      radius: 100000,
+    });
+    map.fitBounds(cityCircle.getBounds());
+    
+  }
+  alert('added');
+
+
+  var myLocation = new google.maps.LatLng(41.835117, -87.627130);
+
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		animation: google.maps.Animation.BOUNCE,
+  });
+  
+  google.maps.event.addListener(marker, 'mouseover', function() {
+    infowindow.open(myMap, marker);
   });
 }
-
+google.maps.event.addDomListener(window, 'load', initMap);
 
 /*
 function init(){
